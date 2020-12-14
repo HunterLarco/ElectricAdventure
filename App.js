7a5c35938caf32fff35bd20221f6a0a3f9adaf68
hunterlarco <hunter@larcolabs.com>
2020-12-14 17:10:42 -0500

    react-native init

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  View,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <View></View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
