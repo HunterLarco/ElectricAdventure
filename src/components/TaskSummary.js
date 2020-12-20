0210a35e09086a18c8287aacb8bad1fbdefa990f
hunterlarco <hunter@larcolabs.com>
2020-12-20 01:06:35 -0500

    task list

--- src/components/TaskSummary.js
+++ src/components/TaskSummary.js
@@ -1,38 +1,28 @@
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5Pro';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Text, View } from 'react-native';
 
 class TaskSummary extends Component {
+  static propTypes = {
+    title: PropTypes.string.required,
+    details: PropTypes.string.required,
+    complete: PropTypes.bool,
+  };
+
   render() {
     return (
       <styles.Host style={this.props.style}>
-        <styles.Icon name="circle" light size={30} />
-        <styles.Content>
-          <styles.Title>{this.props.title}</styles.Title>
-          <styles.Details>{this.props.details}</styles.Details>
-        </styles.Content>
+        <styles.Title>{this.props.title}</styles.Title>
+        <styles.Details>{this.props.details}</styles.Details>
       </styles.Host>
     );
   }
 }
 
 const styles = {
-  Host: styled(View)`
-    flex-direction: row;
-    overflow: hidden;
-    flex-wrap: wrap;
-  `,
-
-  Icon: styled(FontAwesome5)`
-    flex-shrink: 0;
-    margin-right: 10px;
-    margin-top: 4px;
-  `,
-
-  Content: styled(View)`
-    flex: 1;
-  `,
+  Host: styled(View)``,
 
   Title: styled(Text)`
     font-size: 18px;
--- src/features/Home/index.js
+++ src/features/Home/index.js
@@ -1,7 +1,7 @@
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5Pro';
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { ScrollView, Text, View } from 'react-native';
+import { SectionList, Text, View } from 'react-native';
 
 import TaskSummary from '../../components/TaskSummary';
 
@@ -9,43 +9,76 @@ import Theme from '../../Theme';
 
 class Home extends Component {
   render() {
+    const sections = [
+      {
+        title: 'Incomplete Tasks',
+        data: [
+          {
+            title: 'Reserve your spot',
+            details: 'Reserve your spot in the camp before April 14th',
+            complete: false,
+          },
+          {
+            title: 'Pay camp dues',
+            details:
+              "Pay $250 so that the camp can purchase materials like water when we're on the Playa!",
+            complete: false,
+          },
+          {
+            title: 'Purchase tickets',
+            details:
+              'There will be three opportunities to purchase tickets. Each member is required to purchase tickets at each event until they have at least one.',
+            complete: false,
+          },
+          {
+            title: 'Volunteer for camp activities',
+            details:
+              'Each camp member must volunteer to help with camp duties on or off the playa. Helping with pre-build means you can have more free time on the Playa.',
+            complete: false,
+          },
+          {
+            title: 'Schedule to and from travel',
+            details:
+              "You'll need to either carpool with another member or take a bus.",
+            complete: false,
+          },
+        ],
+      },
+      {
+        title: 'Complete Tasks',
+        data: [
+          {
+            title: 'Reserve your spot',
+            details: 'Reserve your spot in the camp before April 14th',
+            complete: true,
+          },
+        ],
+      },
+    ];
+
     return (
       <styles.Host>
         <styles.Summary>
           <styles.SummaryText>Hi Hunter,</styles.SummaryText>
           <styles.SummarySubText>
-            Make sure to RSVP before 4/18
+            Make sure to reserve your spot before April 14
           </styles.SummarySubText>
         </styles.Summary>
 
-        <styles.Tiles>
-          <styles.TilePadding>
-            <styles.Header>
-              <styles.Title>12 Incomplete Tasks</styles.Title>
-            </styles.Header>
-
+        <styles.Tiles
+          sections={sections}
+          keyExtractor={(item) => item.title}
+          ItemSeparatorComponent={() => <styles.Separator />}
+          renderItem={({ item }) => (
             <styles.Task
-              title="Reserve your spot"
-              details="Reserve your spot in the camp before April 14th"
+              title={item.title}
+              details={item.details}
+              complete={item.complete}
             />
-            <styles.Task
-              title="Pay camp dues"
-              details="Pay $250 so that the camp can purchase materials like water when we're on the Playa!"
-            />
-            <styles.Task
-              title="Purchase tickets"
-              details="There will be three opportunities to purchase tickets. Each member is required to purchase tickets at each event until they have at least one."
-            />
-            <styles.Task
-              title="Volunteer for camp activities"
-              details="Each camp member must volunteer to help with camp duties on or off the playa. Helping with pre-build means you can have more free time on the Playa."
-            />
-            <styles.Task
-              title="Schedule to and from travel"
-              details="You'll need to either carpool with another member or take a bus."
-            />
-          </styles.TilePadding>
-        </styles.Tiles>
+          )}
+          renderSectionHeader={({ section: { title } }) => (
+            <styles.Title>{title}</styles.Title>
+          )}></styles.Tiles>
       </styles.Host>
     );
   }
@@ -73,28 +106,25 @@ const styles = {
     font-size: ${Theme.Fonts.HomeSummarySub.Size};
   `,
 
-  Tiles: styled(ScrollView)`
+  Tiles: styled(SectionList)`
     background: #fff;
     flex: 1;
   `,
 
-  TilePadding: styled(View)`
-    padding: 14px;
-  `,
-
-  Header: styled(View)`
-    align-items: center;
-    flex-direction: row;
-  `,
-
   Title: styled(Text)`
+    padding: 14px;
+    background: #fff;
     font-size: 18px;
     text-transform: uppercase;
     color: rgb(10, 10, 10);
   `,
 
   Task: styled(TaskSummary)`
-    margin-top: 20px;
+    padding: 0 14px;
+  `,
+
+  Separator: styled(View)`
+    height: 20px;
   `,
 };
 
